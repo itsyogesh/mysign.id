b9d6a7fde913a8382ebe35472386b2409e0c09e7
Yogesh Kumar <yogeshkumar180592@gmail.com>
2024-02-02 20:45:57 +0530

    Fixed prod issue, remove unused file

--- frontend/src/app/components/compare-slider.tsx
+++ frontend/src/app/components/compare-slider.tsx
@@ -13,7 +13,7 @@ export const CompareSlider: FC<CompareSliderProps> = ({ className, ...rest }) =>
   <ReactCompareSlider
     className={className}
     boundsPadding={10}
-    itemOne={<ReactCompareSliderImage src="/images/mysign-before.jpg" alt="before" />}
+    itemOne={<ReactCompareSliderImage src="/images/mysign-before.jpeg" alt="before" />}
     itemTwo={
       <ReactCompareSliderImage src="/images/mysign-after.png" alt="after" className=" bg-white" />
     }
